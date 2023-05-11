c20f89da914d6f218e8f5cba9ab906d9f2d4be92
tomasitobambito <tomgacek0604@gmail.com>
2023-05-11 15:09:56 +0200

    finished adding basic game functionality

--- ConnectFour.py
+++ ConnectFour.py
@@ -1,6 +1,27 @@
 import helper
 
-board = [['.'] * 6] * 7
+board = [['.'] * 6 for i in range(7)]
+player = True
 
-raise ValueError("That is not a valid move")
+while True:
+    helper.print_board(board)
+
+    while True:
+        columnId = int(input("Choose a column: ")) if player else helper.generate_computer_move(board)
+
+        try:
+            helper.drop_disc(board, columnId, player)
+            break
+        except ValueError:
+            if player:
+                print("That move is not valid")
+
+    if helper.is_board_full(board):
+        helper.print_board(board)
+        print("The board is full, it is a tie")
+        break
+
+    player = not player
+    print(columnId, "was played")
+
 
